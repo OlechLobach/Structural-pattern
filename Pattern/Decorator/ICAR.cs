a38566d1022137aee96de7b1e215db47943ea326
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-24 13:18:16 +0200

    initiall commit

---
+++ Pattern/Decorator/ICAR.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DecoratorPattern
+{
+    public interface ICar
+    {
+        string GetDescription();
+        double GetCost();
+    }
+
+    public class EconomyCar : ICar
+    {
+        public string GetDescription()
+        {
+            return "Economy Car";
+        }
+
+        public double GetCost()
+        {
+            return 20000.0;
+        }
+    }
+
+    public abstract class CarDecorator : ICar
+    {
+        protected ICar car;
+
+        public CarDecorator(ICar car)
+        {
+            this.car = car;
+        }
+
+        public virtual string GetDescription()
+        {
+            return car.GetDescription();
+        }
+
+        public virtual double GetCost()
+        {
+            return car.GetCost();
+        }
+    }
+
+    public class SportsPackage : CarDecorator
+    {
+        public SportsPackage(ICar car) : base(car)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return $"{car.GetDescription()} with Sports Package";
+        }
+
+        public override double GetCost()
+        {
+            return car.GetCost() + 3000.0;
+        }
+    }
+
+    public class LuxuryPackage : CarDecorator
+    {
+        public LuxuryPackage(ICar car) : base(car)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return $"{car.GetDescription()} with Luxury Package";
+        }
+
+        public override double GetCost()
+        {
+            return car.GetCost() + 5000.0;
+        }
+    }
+}
---
+++ Structural pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DecoratorPattern;
+namespace DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar economyCar = new EconomyCar();
+            Console.WriteLine($"Description: {economyCar.GetDescription()}, Cost: ${economyCar.GetCost()}");
+
+            ICar sportsCar = new SportsPackage(economyCar);
+            Console.WriteLine($"Description: {sportsCar.GetDescription()}, Cost: ${sportsCar.GetCost()}");
+
+            ICar luxuryCar = new LuxuryPackage(economyCar);
+            Console.WriteLine($"Description: {luxuryCar.GetDescription()}, Cost: ${luxuryCar.GetCost()}");
+
+            ICar luxurySportsCar = new LuxuryPackage(sportsCar);
+            Console.WriteLine($"Description: {luxurySportsCar.GetDescription()}, Cost: ${luxurySportsCar.GetCost()}");
+        }
+    }
+}
