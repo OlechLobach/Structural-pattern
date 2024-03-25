7f7e6f642fc1964ae0ac56493dc789e58a8e7fbe
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-25 17:02:38 +0200

    initiall commit

---
+++ Pattern/DeliveryService/DeliveryService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DecoratorProxyProject
+{
+    public interface IDeliveryService
+    {
+        string Deliver();
+        TimeSpan GetDeliveryTime();
+        decimal GetCost();
+    }
+
+    public class BasicDeliveryService : IDeliveryService
+    {
+        public string Deliver()
+        {
+            return "Basic delivery service";
+        }
+
+        public TimeSpan GetDeliveryTime()
+        {
+            return TimeSpan.FromDays(3); 
+        }
+
+        public decimal GetCost()
+        {
+            return 0; 
+        }
+    }
+
+   
+    public class DeliveryServiceProxy : IDeliveryService
+    {
+        private readonly IDeliveryService _deliveryService;
+
+        public DeliveryServiceProxy(IDeliveryService deliveryService)
+        {
+            _deliveryService = deliveryService;
+        }
+
+        public string Deliver()
+        {
+            return "Delivery is not available at the moment. Please try again later.";
+        }
+
+        public TimeSpan GetDeliveryTime()
+        {
+            return TimeSpan.Zero;
+        }
+
+        public decimal GetCost()
+        {
+            return 0;
+        }
+    }
+
+    public class TrackingDecorator : IDeliveryService
+    {
+        private readonly IDeliveryService _deliveryService;
+
+        public TrackingDecorator(IDeliveryService deliveryService)
+        {
+            _deliveryService = deliveryService;
+        }
+
+        public string Deliver()
+        {
+            return _deliveryService.Deliver() + ", with tracking";
+        }
+
+        public TimeSpan GetDeliveryTime()
+        {
+            return _deliveryService.GetDeliveryTime().Add(TimeSpan.FromDays(1)); 
+        }
+
+        public decimal GetCost()
+        {
+            return _deliveryService.GetCost() + 3; 
+        }
+    }
+}
---
+++ Structural pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DecoratorProxyProject;
+
+namespace MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDeliveryService deliveryService = new DeliveryServiceProxy(new TrackingDecorator(new BasicDeliveryService()));
+
+            Console.WriteLine(deliveryService.Deliver());
+            Console.WriteLine("Estimated delivery time: " + deliveryService.GetDeliveryTime());
+            Console.WriteLine("Total cost: $" + deliveryService.GetCost());
+        }
+    }
+}
